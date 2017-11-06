6e04e891cfd24f50c297c8d59e1c5bfd34b4956d
Steve Tipton <TIPTONSJ@innovasi.com>
2017-11-06 08:15:11 -0500

    Use a database: Create a migration

--- AspNetCoreTodo/Data/Migrations/20171103181902_AddItems.Designer.cs
+++ AspNetCoreTodo/Data/Migrations/20171103181902_AddItems.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace AspNetCoreTodo.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171103181902_AddItems")]
+    partial class AddItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
